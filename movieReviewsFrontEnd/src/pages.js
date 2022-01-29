811ac496e1cbc7fa549a1f7fa7dff02e3b63415f
Tyler-Coombs <84094311+Tyler-Coombs@users.noreply.github.com>
2022-01-29 19:12:12 -0330

    Adding more styling with react-bootstrap

--- movieReviewsFrontEnd/src/pages.js
+++ movieReviewsFrontEnd/src/pages.js
@@ -5,36 +5,53 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
-import Button from 'react-bootstrap/Button';
 import {Navbar, Nav } from 'react-bootstrap';
-import Alert from 'react-bootstrap/Alert';
+import {Form, FormControl, FormGroup, ControlLabel, HelpBlock, Button} from 'react-bootstrap';
+import { Collapse } from "react-bootstrap";
 
 
 
 export function Home({movies, setMovies}) {
     function Movie( {name, date, actors, poster, rating, onRemove = f => f} ) {
+        const [open, setOpen] = useState(false);
+
         return (
           <>
             <Col xs={3}>
-              <Row>
-                <h3 class="p-3 mb-1 bg-dark text-white">{name}</h3>
-              </Row>
+                
+                
+                <Row>
+                    <h3 >{name}</h3>
+                </Row>
               <Row>
                 <Image rounded="true" src={poster} alt={name + " Movie Poster"}
                 width={900} length={1200}>
                 </Image>
               </Row>
+              <Button
+                    onClick={() => setOpen(!open)}
+                    aria-controls="example-collapse-text"
+                    aria-expanded={open}
+                    size='lg'
+                >
+                    Movie Info
+                </Button>
+                <Collapse in={open}>
+                    <div id="example-collapse-text">
               <Row>
-                <h3 class="p-3 mb-1 bg-dark text-white">Release Date: {date}</h3>
+                <p className="fs-4 text-start">Release Date: {date}</p>
               </Row>
               <Row>
-                <h3 class="p-3 mb-1 bg-dark text-white">Lead Actors: {(actors).join(", ")}</h3>
+                <p className="fs-4 text-start">Lead Actors: {(actors).join(", ")}</p>
               </Row>
               <Row>
-                <h3 class="p-3 mb-1 bg-dark text-white">Rating: {rating} Stars</h3>
+                <p className="fs-4 text-start">Rating: {rating} Stars</p>
               </Row>
-              <Row>
-                <Button onClick={() => onRemove(name)}>Remove</Button>
+              
+              </div>
+              </Collapse>
+              <Row xs={3} className="justify-content-md-center">
+                <Button variant='danger' onClick={() => onRemove(name)}>Remove</Button>
               </Row>
             </Col>
           </>
@@ -51,11 +68,15 @@ export function Home({movies, setMovies}) {
             </>
         );
     }
-
+    
     return (
         <>
             <Header />
+            <div className="bg-secondary text-white">
             <Container fluid>
+                <Row>
+                    <p className="h2 "></p>
+                </Row>
                 <Row className="justify-content-center">
                     <MovieList 
                         movies={movies} 
@@ -65,6 +86,7 @@ export function Home({movies, setMovies}) {
                     }} />
                 </Row>
             </Container>
+            </div>
             <Footer year={new Date().getFullYear()}/>
         </>
     );
@@ -95,34 +117,39 @@ export function AddReview({movies, setMovies}) {
 
         return (
             <>
-                <form onSubmit={submit}>
-                    <div>
-                        <label>Movie Title:<input ref={txtName} type="text" required /></label>
-                    </div>
-                    <div>
-                        <label>Release Date:<input ref={txtDate} type="text" required /></label>
-                    </div>
-                    <div>
-                        <label>Lead Cast:<input ref={txtActors} type="text" required /></label>
-                    </div>
-                    <div>
-                        <label>Movie Poster:<input type="file" accept=".png,.jfif,.jpg,.jpeg"
-                        onChange = {e => setPoster(URL.createObjectURL(e.target.files[0]))} required /></label>
-                    </div>
-                    <div>
-                    <label>Rating:<input ref={txtRating} type="text" required/></label>
-                    </div>
-                    <div>
-                        <input type="submit" value="Submit"></input>
-                    </div>
-                </form>
+            <Container>
+                <Row className="justify-content-center">
+                    <Col>
+                        <Form onSubmit={submit}>
+                            <div>
+                                <label className="form-label">Movie Poster:<input className="form-control" type="file" accept=".png,.jfif,.jpg,.jpeg"
+                                onChange = {e => setPoster(URL.createObjectURL(e.target.files[0]))} required /></label>
+                            </div>
+                            <div>
+                                <label className="form-label">Movie Title:<input className="form-control" ref={txtName} type="text" required /></label>
+                            </div>
+                            <div>
+                                <label className="form-label">Release Date:<input className="form-control" ref={txtDate} type="text" required /></label>
+                            </div>
+                            <div>
+                                <label className="form-label">Lead Cast:<input className="form-control" ref={txtActors} type="text" required /></label>
+                            </div>
+                            <div>
+                            <label className="form-label">Rating:<input className="form-control" ref={txtRating} type="text" required/></label>
+                            </div>
+                            <Button variant="primary" type="submit">Submit</Button>
+                        </Form>
+                    </Col>
+                </Row>
+            </Container>
             </>
         );
     }
 
     return (
         <>
-            <h1>Add A Review</h1>
+        <header className="bg-dark text-white">
+            <h1 className='display-3'>Add A Review</h1>
             <div>
                 <Navbar bg="dark" variant="dark">
                     <Container>
@@ -134,12 +161,15 @@ export function AddReview({movies, setMovies}) {
                     </Container>
                 </Navbar>
             </div>
-            <br></br>
+            </header>
+            
+            <div className="bg-secondary text-white">
             <AddReviewForm onNewMovie={(name, date, actors, poster, rating) => {
                 const newMovies= [...movies, {name, date, actors, poster, rating}];
                 setMovies(newMovies)
             }}
             />
+            </div>
         </>
     );
 }
@@ -147,8 +177,8 @@ export function AddReview({movies, setMovies}) {
 function Header() {
     return (
     <>
-      <header>
-        <h1 class='display-2'>Movie Reviews</h1>
+      <header className="bg-dark text-white">
+        <h1 className='display-3'>Movie Reviews</h1>
         <div>
         <Navbar bg="dark" variant="dark">
             <Container className="justify-content-center">
@@ -168,7 +198,7 @@ function Header() {
 function Footer(props) {
     return (
         <>
-        <footer>
+        <footer className="bg-secondary text-white">
             <p>Copyright {props.year}</p>
         </footer>
         </>
